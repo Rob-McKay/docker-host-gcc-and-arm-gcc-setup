fe35944eafd73e9daaea51940318ea6de5fd2715
Rob McKay <robert.mckay@global.komatsu>
2025-09-03 10:53:59 +0100

    Removed SBOM scan

--- build-with-tidy.sh
+++ build-with-tidy.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-#docker build -t rmckaykm/gcc-and-arm-gcc-and-clang-tidy:latest -f src/Dockerfile-with-clang-tidy .
+docker build -t rmckaykm/gcc-and-arm-gcc-and-clang-tidy:latest -f src/Dockerfile-with-clang-tidy .
 
-docker build -t rmckaykm/gcc-and-arm-gcc-and-clang-tidy-zs:latest -f src/Dockerfile-with-clang-tidy-zs .
+#docker build -t rmckaykm/gcc-and-arm-gcc-and-clang-tidy-zs:latest -f src/Dockerfile-with-clang-tidy-zs .
 
 #docker run --rm -it -V $(pwd):/home/build rmckaykm/gcc-10host-arm:latest apt list --installed
